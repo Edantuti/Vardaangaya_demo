fa05e3255a614a997334b8a1eb005cee8a920695
edantuti <edantuti@gmail.com>
2023-06-03 15:17:42 +0530

    Changed the gallery section

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -14,75 +14,31 @@ import Image11 from "../assets/gallery/image11.jpg"
 function Gallery(){
 
 	return (
-		<>
-		<h2 className="text-center text-6xl">Gallery</h2>
-		<div id="default-carousel" className="relative w-full" data-carousel="slide">
-    
-    <div className="relative h-screen m-4 overflow-hidden rounded-lg bg-slate-200">
-        
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image1} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+        <>
+        <h2 className="text-center text-5xl my-7">Gallery</h2>
+		<div className="flex md:flex-row flex-col gap-4 m-6">
+            <div className="flex flex-col gap-4">
+                <img className="rounded shadow-md shadow-slate-300" src={Image1} alt="image 1"/>
+                <img className="rounded shadow-md shadow-slate-300 md:h-auto h-64" src={Image2} alt="image 2"/>
+            </div>
+            <div className="flex flex-col  gap-4">
+                <img className="rounded shadow-md shadow-slate-300  md:h-auto h-64" src={Image3} alt="image 3"/>
+                <img className="rounded shadow-md shadow-slate-300" src={Image4} alt="image 4"/>
+            </div>
+            <div className="flex flex-col  gap-4">
+                <img className="rounded shadow-md shadow-slate-300 " src={Image5} alt="image 5"/>
+                <img className="rounded shadow-md shadow-slate-300 " src={Image6} alt="image 6"/>
+            </div>
+            <div className="flex flex-col  gap-4">
+                <img className="rounded shadow-md shadow-slate-300" src={Image7} alt="image 7"/>
+                <img className="rounded shadow-md shadow-slate-300" src={Image8} alt="image 8"/>
+            </div>
+            <div className="flex flex-col  gap-4">
+                <img className="rounded shadow-md shadow-slate-300 md:h-auto h-64" src={Image9} alt="image 9"/>
+                <img className="rounded shadow-md shadow-slate-300" src={Image11} alt="image 11"/>
+            </div>
         </div>
-        
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image2} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image3} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image4} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image5} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image6} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image7} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image8} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image9} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Image11} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-    </div>
-    
-    <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 6" data-carousel-slide-to="5"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 7" data-carousel-slide-to="6"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 8" data-carousel-slide-to="7"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 9" data-carousel-slide-to="8"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 10" data-carousel-slide-to="9"></button>
-    </div>
-    
-    <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
-            <span className="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
-            <span className="sr-only">Next</span>
-        </span>
-    </button>
-</div>
-		</>
+        </>
 		)
 }
 
