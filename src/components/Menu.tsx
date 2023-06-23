5831cf507ea4bc4c7f1a42f028a02ebb5ae7d1a3
Edantuti <edantuti@gmail.com>
2023-06-23 15:05:34 +0530

    Added a feature of automatic closing when clicked

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,11 @@
-import { FC } from "react"
+import { Dispatch, FC, SetStateAction } from "react"
 import { Link} from "react-router-dom"
 
 import Logo from "../assets/Site_Icon.png"
 
 interface IMenuProps{
     visible:boolean
+    changeVisibility: Dispatch<SetStateAction<boolean>>
 }
 
 const Menu: FC<IMenuProps> = (props): JSX.Element => {
@@ -14,10 +15,10 @@ const Menu: FC<IMenuProps> = (props): JSX.Element => {
             <nav className={props.visible?"fixed mb-4 sm:w-96 w-full bg-white h-full top-0 right-0 transition-all shadow-lg":"fixed sm:w-96 w-96 mb-4 bg-white h-full top-0 -right-96 transition-all"}>
                 <img src={Logo} alt="" className="w-24 h-24 mx-auto my-28"/>
                 <div className="grid grid-flow-row mx-auto">
-                    <Link to="/" className="px-2 py-1">Home</Link>
-                    <Link to="/speciality"className="px-2 py-1">Speciality</Link>
-                    <Link to="/updates" className="px-2 py-1">Updates</Link>
-                    <Link to="/contact"className="px-2 py-1">Contact</Link>
+                    <Link to="/" onClick={()=>{props.changeVisibility(!props.visible)}} className="px-2 py-1">Home</Link>
+                    <Link to="/speciality" onClick={()=>{props.changeVisibility(!props.visible)}} className="px-2 py-1">Speciality</Link>
+                    <Link to="/updates" onClick={()=>{props.changeVisibility(!props.visible)}} className="px-2 py-1">Updates</Link>
+                    <Link to="/contact" onClick={()=>{props.changeVisibility(!props.visible)}} className="px-2 py-1">Contact</Link>
                 </div>
             </nav>
         </>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,7 +16,7 @@ function NavBar() {
 				<h1 className="text-lg text-center md:text-3xl font-semibold flex-1">VARDAAN HOSPITAL & MATERNITY CENTER, GAYA</h1>
 				{!drawerState?<GiHamburgerMenu onClick={()=>changeDrawerState(!drawerState)} className="flex-2 w-5 h-5 absolute right-4 top-12 z-1"/>:<RiCloseFill onClick={()=>changeDrawerState(!drawerState)} className="flex-2 w-5 h-5 fixed right-4 top-12 z-10"/>}
 			</div>
-			<Menu visible={drawerState} />
+			<Menu visible={drawerState} changeVisibility={changeDrawerState} />
 			<a href="https://goo.gl/maps/GQ7vDQbR9p3r1ChK6" target="_blank" rel="noopner" className="grid"><span className="flex items-center mx-auto">Gewalbigha, Gaya, Bihar 823001  <FiExternalLink/></span></a>
 			<div className="text-[12px] md:text-md text-center grid grid-flow-col gap-5 w-52 mx-auto my-5 uppercase font-medium">  <a href="tel:+916203188133" className="p-3 border-2 border-black hover:border-b-8 hover:pb-1 transition-all ">Phone</a><a href="mailto:contact@vardaangaya.com" className="p-3 border-2 border-black hover:border-b-8 transition-all hover:pb-1">Email</a></div>
 		</header>
