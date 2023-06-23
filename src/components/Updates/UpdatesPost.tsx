86917fe0a20625c770293d52fdc7f45be41241b7
Edantuti <edantuti@gmail.com>
2023-06-23 15:08:14 +0530

    Changed the link in the update section for the update post

--- src/components/Updates/UpdatesPost.tsx
+++ src/components/Updates/UpdatesPost.tsx
@@ -18,7 +18,7 @@ const UpdatesPost: FC<IUpdatesPost> = ({post}): JSX.Element =>{
         <>
             <div className="mx-5 mt-5 grid h-[30%] sm:h-[20%] gap-8 overflow-hidden rounded md:grid-cols-2 bg-slate-50">
 				<img className="row-span-2 rounded-l h-full md:w-full md:my-auto  bg-slate-10" src={post.image_url} alt={post.image_alt} />
-				<div className="mt-10 flex flex-col gap-5">
+				<div className="mt-10 flex flex-col gap-5 mx-5">
                     <h2 className="row-span-1 rounded my-auto text-2xl font-roboto ">{post.title}</h2>
 				    <p className="rounded font-mono text-sm line-clamp-5">{post.description}</p>
                     <span className="flex  items-center text-blue-600"><a href={post.sources}>Source</a> <FiExternalLink/></span>
